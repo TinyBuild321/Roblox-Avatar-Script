cbb21bd36be4c407d4d44ba17edc50dee22ec7ee
Zack <78792046+TinyBuild321@users.noreply.github.com>
2022-06-29 22:28:42 +0800

    Add files via upload

---
+++ Custom Face Giver.lua
@@ -0,0 +1,5 @@
+local Rbx = game:GetService("Players").LocalPlayer
+local Rbx = Rbx.Character
+local Rbx = Rbx:FindFirstChild("Head")
+local Rbx = Rbx:FindFirstChild("face") or Rbx:FindFirstChild("Face")
+Rbx.Texture = "rbxassetid://0" -- replace 0 with decal asset id
